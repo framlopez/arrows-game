c20017e16ff427ea4d95058c52f444660aa0c3b9
Francisco tomas Lopez <francisco.lopez@mercadolibre.com>
2020-01-13 10:31:13 -0300

    Updates

---
+++ precache-manifest.291521c97837aa90654caa55bec601f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "191b576accfac32755e80698e18bc517",
+    "url": "/arrows-game/index.html"
+  },
+  {
+    "revision": "80fc5e1d6ca672de74ba",
+    "url": "/arrows-game/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "887e1fe72c20266b2c7d",
+    "url": "/arrows-game/static/css/main.b2cce9a8.chunk.css"
+  },
+  {
+    "revision": "80fc5e1d6ca672de74ba",
+    "url": "/arrows-game/static/js/2.dcbcd931.chunk.js"
+  },
+  {
+    "revision": "5f8cf23863548b75359d029f3a6b7736",
+    "url": "/arrows-game/static/js/2.dcbcd931.chunk.js.LICENSE"
+  },
+  {
+    "revision": "887e1fe72c20266b2c7d",
+    "url": "/arrows-game/static/js/main.914d3320.chunk.js"
+  },
+  {
+    "revision": "6ca1c3cf98886285c697",
+    "url": "/arrows-game/static/js/runtime-main.54420f94.js"
+  }
+]);
